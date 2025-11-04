28de752fdc2141f489cd1dd1561d93f2e2ecb4a2
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-04 22:52:43 +0200

    update

--- app/info/contact/page.tsx
+++ app/info/contact/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 
 function ContactPage() {
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, velit! Odio nobis illo magni iure est dolor praesentium magnam molestiae aliquam temporibus id tempore amet blanditiis, quidem cum porro delectus.
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum eaque tempore recusandae. Dolorem doloremque eveniet, mollitia tenetur fugiat qui possimus et quod. Voluptas ipsam hic, nobis doloribus dignissimos ea quis.
   return (
     <div>
       <h1 className="text-7xl">Contact Page</h1>
@@ -9,7 +11,7 @@ function ContactPage() {
         back home
       </Link>
       <link rel="stylesheet" href="" />
-      
+
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -18,3 +18,5 @@ function Homepage() {
 export default Homepage;
 
 link
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur reiciendis ducimus corrupti expedita dolores odio quisquam. Itaque dignissimos iste hic facere quam voluptate sapiente expedita id, unde officia voluptates architecto?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, officiis harum alias voluptate culpa itaque repudiandae eius nulla optio dolore illum perferendis non atque tenetur molestias. Excepturi labore soluta illum.
