b84c5a19696d7c2ada9fd7f79598bb1eed6af92b
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-11 20:34:47 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { link } from "node:fs";
 
 
 
@@ -10,6 +9,7 @@ function Homepage() {
       
       <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">
       about page
+      
       </Link>
 
     </div>
@@ -17,6 +17,3 @@ function Homepage() {
 }
 export default Homepage;
 
-link
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur reiciendis ducimus corrupti expedita dolores odio quisquam. Itaque dignissimos iste hic facere quam voluptate sapiente expedita id, unde officia voluptates architecto?
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, officiis harum alias voluptate culpa itaque repudiandae eius nulla optio dolore illum perferendis non atque tenetur molestias. Excepturi labore soluta illum.
