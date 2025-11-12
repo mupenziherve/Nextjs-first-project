947224fd71786bd8e0a2ff4f2425ec3e8d01ffac
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-12 10:40:59 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 import './globals.css';
 import { Navbar } from '@/components/Navbar';
-export default function RootLayout({children}:{children:React.ReactNode}){
-return <html lang='eng'>
-<body>
-<Navbar/>
-<main className=''>
+import {  Inter} from 'next/font/google';
 
-</main>
-  {children}
-</body>
-
-</html>
+const inter = Inter({ subsets: ['latin' ] });
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+      <html lang="en">
+        <body className={inter.className}>
+          <Navbar />
+          <main className="max-w-3xl mx-auto py-10">{children}</main>
+        </body>
+      </html>
+    );
 
 }
+
+
