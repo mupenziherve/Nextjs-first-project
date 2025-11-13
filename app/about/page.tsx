c03dbaa5110aea8cd09c672e859f7821577c068c
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-13 22:57:24 +0200

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,39 +3,7 @@ import Link from "next/link";
 function AboutPage() {
   return (
     <div>
-      <h1 className="text-7xl" 'use client'
-      
-      import { useState } from "react";
-      
-      const Counter = () => {
-           
-         const [count,setCount] = useState(0);
-        return <div className="flex flex-col items-center w-[100px]">
-        <p className="text-5xl font-bold">{count}</p>
-        <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
-        text-white px-4 py-2 mt-4">Increase</button>
-        </div>
-      
-      };
-      
-      export default Counter;
-      'use client'
-      
-      import { useState } from "react";
-      
-      const Counter = () => {
-           
-         const [count,setCount] = useState(0);
-        return <div className="flex flex-col items-center w-[100px]">
-        <p className="text-5xl font-bold">{count}</p>
-        <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
-        text-white px-4 py-2 mt-4">Increase</button>
-        </div>
-      
-      };
-      
-      export default Counter;
-      >About Page</h1>
+      <h1 className="text-7xl" >About Page</h1>
     </div>
 
   )
