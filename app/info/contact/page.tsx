c8d03a4993902ef58f3b6642c5248a0cd0489c02
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-13 22:56:30 +0200

    update

--- app/info/contact/page.tsx
+++ app/info/contact/page.tsx
@@ -5,7 +5,39 @@ function ContactPage() {
     <div>
       <h1 className="text-7xl">Contact Page</h1>
 
-      <Link href="/" className="text-xl text-blue-500 inline-block mt-8">
+      <Link href="/" className="text-x'use client'
+      
+      import { useState } from "react";
+      
+      const Counter = () => {
+           
+         const [count,setCount] = useState(0);
+        return <div className="flex flex-col items-center w-[100px]">
+        <p className="text-5xl font-bold">{count}</p>
+        <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
+        text-white px-4 py-2 mt-4">Increase</button>
+        </div>
+      
+      };
+      
+      export default Counter;
+      'use client'
+      
+      import { useState } from "react";
+      
+      const Counter = () => {
+           
+         const [count,setCount] = useState(0);
+        return <div className="flex flex-col items-center w-[100px]">
+        <p className="text-5xl font-bold">{count}</p>
+        <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
+        text-white px-4 py-2 mt-4">Increase</button>
+        </div>
+      
+      };
+      
+      export default Counter;
+      l text-blue-500 inline-block mt-8">
         back home
       </Link>
       <link rel="stylesheet" href="" />
