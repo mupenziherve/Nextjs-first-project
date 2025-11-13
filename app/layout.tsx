a4812fedaf32abe4a8af0481065987c227827764
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-13 22:55:52 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,39 @@
 import './globals.css';
 import { Navbar } from '@/components/Navbar';
 import type { Metadata } from 'next';
-import {  Inter} from 'next/font/google';
+import {  Inter} from 'next'use client'
+
+import { useState } from "react";
+
+const Counter = () => {
+     
+   const [count,setCount] = useState(0);
+  return <div className="flex flex-col items-center w-[100px]">
+  <p className="text-5xl font-bold">{count}</p>
+  <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
+  text-white px-4 py-2 mt-4">Increase</button>
+  </div>
+
+};
+
+export default Counter;
+'use client'
+
+import { useState } from "react";
+
+const Counter = () => {
+     
+   const [count,setCount] = useState(0);
+  return <div className="flex flex-col items-center w-[100px]">
+  <p className="text-5xl font-bold">{count}</p>
+  <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
+  text-white px-4 py-2 mt-4">Increase</button>
+  </div>
+
+};
+
+export default Counter;
+/font/google';
 
 const inter = Inter({ subsets: ['latin' ] });
 
