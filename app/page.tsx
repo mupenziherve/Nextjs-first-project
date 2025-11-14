7e8ace00657bef76bb251a0a6b30f84db99b2479
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-14 12:25:21 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,7 @@ import Link from "next/link";
 
 function Homepage() {
   return (
-    <div>'use client'
-    
-    import { useState } from "react";
-    
-    const Counter = () => {
-         
-       const [count,setCount] = useState(0);
-      return <div className="flex flex-col items-center w-[100px]">
-      <p className="text-5xl font-bold">{count}</p>
-      <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
-      text-white px-4 py-2 mt-4">Increase</button>
-      </div>
-    
-    };
-    
-    export default Counter;
-    
+    <div>
       <h1 className="text-7xl">Home Page</h1>
       
       <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,6 @@
 import Link from "next/link";
 
-export const'use client'
-
-import { useState } from "react";
-
-const Counter = () => {
-     
-   const [count,setCount] = useState(0);
-  return <div className="flex flex-col items-center w-[100px]">
-  <p className="text-5xl font-bold">{count}</p>
-  <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
-  text-white px-4 py-2 mt-4">Increase</button>
-  </div>
-
-};
-
-export default Counter;
-'use client'
-
-import { useState } from "react";
-
-const Counter = () => {
-     
-   const [count,setCount] = useState(0);
-  return <div className="flex flex-col items-center w-[100px]">
-  <p className="text-5xl font-bold">{count}</p>
-  <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
-  text-white px-4 py-2 mt-4">Increase</button>
-  </div>
-
-};
-
-export default Counter;
- Navbar = () => {
+export const Navbar = () => {
   return (
     <nav className="max-w-3xl mx-auto py-4 flex gap-x-4" >
       <Link href="/">Home</Link>
