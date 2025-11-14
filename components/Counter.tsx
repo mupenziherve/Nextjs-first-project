35f9248eef7cee4377f89771f8c30a93f4a778cd
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-14 23:46:26 +0200

    update

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -14,11 +14,3 @@ const Counter = () => {
 };
 
 export default Counter;
-function InfoPage() {
-  return (
-    <div>
-      <h1 className="text-7xl">Info Page</h1>
-    </div>
-  )
-}
-export default InfoPage
