1b200bd04a30f12498fd4dcedd42f79ecb1afc2c
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-18 16:50:03 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,31 +22,7 @@ export default function RootLayout({
     return (
       <html lang="en">
         <body className={inter.className}>
-          <Navbar />const url = 'https://www.course-api.com/react-tours-project';
-
-type Tour = {
-  id: string;
-  name: string;
-  info: string;
-  image: string;
-  price: string;
-};
-
-async function ToursPage() {
-  const response = await fetch(url);
-  const data: Tour[] = await response.json();
-  console.log(data);
-  return (
-    <section>
-      <h1 className='text-3xl mb-4'>Tours</h1>
-
-      {data.map((tour) => {
-        return <h2 key={tour.id}>{tour.name}</h2>;
-      })}
-    </section>
-  );
-}
-export default ToursPage;
+          <Navbar />
           <main className="max-w-3xl mx-auto py-10">{children}</main>
         </body>
       </html>
--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,58 +1,10 @@
 'use client'
 
 import { useState } from "react";
-const url = 'https://www.course-api.com/react-tours-project'
 
-type Tour = {
-  id: string
-  name: string
-  info: string
-  image: string
-  price: string
-}
-
-async function ToursPage() {
-  const response = await fetch(url)
-  const data: Tour[] = await response.json()
-  console.log(data)
-  return (
-    <section>
-      <h1 className="text-3xl mb-4">Tours</h1>
-
-      {data.map((tour) => {
-        return <h2 key={tour.id}>{tour.name}</h2>
-      })}
-    </section>
-  )
-}
-export default ToursPageconst url = 'https://www.course-api.com/react-tours-project';
-
-type Tour = {
-  id: string;
-  name: string;
-  info: string;
-  image: string;
-  price: string;
-};
-
-async function ToursPage() {
-  const response = await fetch(url);
-  const data: Tour[] = await response.json();
-  console.log(data);
-  return (
-    <section>
-      <h1 className='text-3xl mb-4'>Tours</h1>
-
-      {data.map((tour) => {
-        return <h2 key={tour.id}>{tour.name}</h2>;
-      })}
-    </section>
-  );
-}
-export default ToursPage;
 const Counter = () => {
-     
-   const [count,setCount] = useState(0);
+ const [count,setCount] = useState(0);
+
   return <div className="flex flex-col items-center w-[100px]">
   <p className="text-5xl font-bold">{count}</p>
   <button onClick={() => setCount(count + 1)} className="bg-blue-900 rounded
